4dc36ab4a2215103a24fc17af937e1b6e8743c65
Carlos Galdino <carloshsgaldino@gmail.com>
2014-10-21 20:45:18 -0200

    Fix output.
    1. Alfred requires that the resulting XML should have HTML characters
    escaped.
    
    2. `&` should be escaped just like `+` was on d05c642b61.

--- script.rb
+++ script.rb
@@ -1,5 +1,11 @@
 require 'net/http'
 require 'json'
+require 'erb'
+
+substitutions = {
+  '+' => '%2B',
+  '&' => '%26',
+}
 
 def item_xml(options = {})
   <<-ITEM
@@ -11,13 +17,21 @@ def item_xml(options = {})
   ITEM
 end
 
-uri = URI.parse(URI.encode("http://www.haskell.org/hoogle/?mode=json&hoogle=#{ARGV.first}").gsub('+', '%2B'))
+def escape_html(str)
+  ERB::Util.html_escape(str)
+end
+
+arg = URI.escape(ARGV.first)
+
+substitutions.each { |k, v| arg.gsub!(k, v) }
+
+uri = URI.parse("http://www.haskell.org/hoogle/?mode=json&hoogle=#{arg}")
 
 response = JSON.parse(Net::HTTP.get(uri))
 
 items = response['results'].map do |result|
-  item_xml({ :arg => result['location'], :title => result['self'],
-             :subtitle => result['docs'], :uid => result['location'] })
+  item_xml({ :arg => escape_html(result['location']), :title => escape_html(result['self']),
+             :subtitle => escape_html(result['docs']), :uid => escape_html(result['location']) })
 end.join
 
 output = "<?xml version='1.0'?>\n<items>\n#{items}</items>"
